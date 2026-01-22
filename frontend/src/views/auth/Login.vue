64306baff183e0f728f621a08a4f4dd08012ca2b
linjiahang <linjiahang@atia.com>
2026-01-22 11:46:49 +0800

    style(logo): 更换应用图标为atia
    - 将登录页面的weknora图标替换为atia图标
    - 将菜单组件中的weknora图标替换为atia图标
    - 移除用户菜单中的API文档、官网和GitHub链接选项

--- frontend/src/views/auth/Login.vue
+++ frontend/src/views/auth/Login.vue
@@ -180,7 +180,7 @@
 
     <!-- Logo - Top Left -->
     <a href="https://github.com/Tencent/WeKnora" target="_blank" class="header-logo" :title="$t('common.github')">
-      <img src="@/assets/img/weknora.png" alt="WeKnora" class="logo-image" />
+      <img src="@/assets/img/atia.png" alt="WeKnora" class="logo-image" />
     </a>
 
     <!-- Header Links - Top Right -->
